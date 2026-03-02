167665e9d6d1739150a7276f5bc27b27928cb58d
ShadowSeick <alcolka@gmail.com>
2026-03-02 20:29:15 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"net"
 	"os"
 )
@@ -9,15 +9,34 @@ import (
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		slog.Error("failed to bind to port 6379", "err", err)
 		os.Exit(1)
 	}
+	slog.Info("TCP connection opened in port 6379")
+
 	conn, err := l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		slog.Error("error accepting connection", "err", err)
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
+	defer conn.Close()
+	incomingData := make([]byte, 256)
+	for {
+		n, err := conn.Read(incomingData)
+		if err != nil {
+			slog.Error("error reading data from connection", "err", err)
+			return
+		}
+
+		if n == 0 {
+			continue
+		}
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			slog.Error("error writing data to connection", "err", err)
+			return
+		}
+	}
 }
